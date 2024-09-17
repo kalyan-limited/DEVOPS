c7281af58d04e72d35963a008decf4ce75dc7747
kalyan-hub <nsvkalyan@gmail.com>
2024-09-17 05:37:09 -0700

    fifth

---
+++ test.sh
@@ -0,0 +1,19 @@
+echo "Welcome to my world"
+name="John"
+echo "HEllO, $name"
+date="17-09-2024"
+echo " Date is $date"
+cost=21000.00
+echo "Rent is $cost"
+a=4
+b=5
+result=$((a+b))
+echo "Result= $result"
+c=$((a-b))
+echo "sub $c"
+d=$((a*b))
+echo "mul $d"
+e=$((a/b))
+echo "Div $e"
+f=$((a*b))
+echo "pow $f"
